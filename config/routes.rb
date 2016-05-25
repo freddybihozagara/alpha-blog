fad8699c4034b02ded5bd82e7b60bd251db13b86
Rails blog project <frdb193@gmail.com>
2016-05-25 00:27:39 +0000

    create template, action, route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles/new'
+
+  resources :articles
+  
   get 'about' => 'pages#about'
 
   root 'pages#home'
+ 
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
