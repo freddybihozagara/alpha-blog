120ea8de934a78d8a94b2eef47155288b6f24bde
Rails blog project <frdb193@gmail.com>
2016-05-30 16:12:11 +0000

    add session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,28 @@
 class SessionsController < ApplicationController
 
 def new
+    #@session = 
+    
 end
 
 def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+        session[:user_id] = user.id
+        flash[:success] = "You are successfully logged in"
+        redirect_to users_path(user)
+    else
+        flash.now[:danger] = "check your username and password and try again"
+        render 'new'
+        
+    end
 end
 
 def destroy
+    session[:user_id] = nil
+    flash[:success] = "You are successfully logged out"
+    redirect_to root_path
+    
 end
 
 
